d4fdacf47150eb7a09d23f901f628989a0bf7e91
ruudgithub <info@pum.nl>
2014-01-20 10:05:19 +0100

    Added import of csv file in code and try to read it out

--- postalcode.php
+++ postalcode.php
@@ -68,3 +68,76 @@ function postalcode_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 function postalcode_civicrm_managed(&$entities) {
   return _postalcode_civix_civicrm_managed($entities);
 }
+
+/**
+ * Implementation of hook_civicrm_navigationMenu
+ *
+ * Discloses additional menu options to trigger import functions through page urls
+  */
+function postalcode_civicrm_navigationMenu( &$params ) {
+	//  Get the maximum key of $params
+	$maxKey = ( max( array_keys($params) ) );
+	
+	$params[$maxKey+1] = array (
+		'attributes' => array (
+			'label'      => 'Config',
+			'name'       => 'Config',
+			'url'        => null,
+			'permission' => 'administer CiviCRM',
+			'operator'   => null,
+			'separator'  => null,
+			'parentID'   => null,
+			'navID'      => $maxKey+1,
+			'active'     => 1
+			),
+		'child' =>  array (
+			'1' => array (
+				'attributes' => array (
+					'label'      => 'Import Postalcode Table',
+					'name'       => 'Postalcodetable_import',
+					'url'        => 'civicrm/Postalcodetable_import',
+					'permission' => 'administer CiviCRM',
+					'operator'   => null,
+					'separator'  => 1,
+					'parentID'   => $maxKey+1,
+					'navID'      => 1,
+					'active'     => 1
+					),
+				'child' => null
+				),
+			'2' => array (
+				'attributes' => array (
+					'label'      => 'Clear Postalcode Table',
+					'name'       => 'Postalcodetable_clear',
+					'url'        => 'civicrm/Postalcodetable_clear',
+					'permission' => 'administer CiviCRM',
+					'operator'   => null,
+					'separator'  => 1,
+					'parentID'   => $maxKey+1,
+					'navID'      => 1,
+					'active'     => 1
+					),
+				'child' => null
+				),
+			)
+		);
+}
+
+function postalcode_civicrm_postProcess( $formName, &$form ) {
+	if ($formName == 'CRM_Contact_Form_Contact' ) {
+		//dpm("Submitted values: ");
+		//dpm($form->_submitValues);
+		
+		if (array_key_exists('address',$form->_submitValues)) {
+			$total_addresses = count($form->_submitValues['address']);
+			//dpm('Number of addresses: '.$total_addresses);
+    	}
+    	
+		//Note: $form->_submitValues starts with key 1
+    	for ($i = 1; $i <= count($form->_submitValues['address']); $i++) {
+    		//dpm($form->_submitValues['address'][$i]['postal_code']);
+    	}
+    	
+    	//Postcode.nl API: https://api.postcode.nl/rest/addresses/2012ES/30		//authentication required
+   	}
+}
