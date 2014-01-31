7a3c5aef5bf475225f791da916d1a1c0c7060615
ruudgithub <info@pum.nl>
2014-01-31 01:50:00 +0100

    Import working & old postalcode table will be backupped on import

--- CRM/Postalcode/Page/Postalcodetableimport.php
+++ CRM/Postalcode/Page/Postalcodetableimport.php
@@ -1,67 +1,74 @@
 <?php
 
 require_once 'CRM/Core/Page.php';
-//require_once '/../functions/dbconnect.inc.php';
 
 class CRM_Postalcode_Page_Postalcodetableimport extends CRM_Core_Page {
   function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('Postalcode table import'));
 
-//$conn = new CRM_Postalcode_Page_Dbconnect();
+	$file_pctable = dirname(__FILE__).'/../../../verkleinde_postcode_tabel.txt';
 	
-    if (($handle = fopen(dirname(__FILE__).'/../../../verkleinde_postcode_tabel.txt','r')) !== FALSE) {
+    if (($handle = fopen($file_pctable,'r')) !== FALSE) {
 		$dtstamp = date('Y_m_d__H_i_s');
 		
-		//First drop existing table
-		//Needs adding checksum code like if database exists etc., but couldn't get that to work at the moment
+		/**
+		 * First backup existing table, then create new table
+		 * 
+		 * @todo On install of the module, check if the table already exists by some other module
+		 * @todo On import check if the table exists & have access to it, otherwise we get an error on renaming...
+		 */
 		
-		//$sql_0 = "SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = `devcivi` AND table_name = `pum_postalcodes`;";
-		$sql_0 = "SHOW TABLES FROM devcivi_civicrm";
+		$qRenameTable_pc = "RENAME TABLE nl_pum_postalcodes TO nl_pum_postalcodes_".$dtstamp;
 		
-		$sql_1 = "RENAME TABLE devcivi_civicrm.pum_postalcodes TO pum_postalcodes_".$dtstamp;
-		$sql_2 = "CREATE TABLE IF NOT EXISTS `devcivi_civicrm.pum_postalcodes` (
-					`frompc` int(11) NOT NULL,
-					`topc` int(11) NOT NULL,
-					`time` int(11) NOT NULL,
-					`distance` mediumint(9) NOT NULL
-					) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+		$qCreateTable_pc = "CREATE TABLE IF NOT EXISTS `nl_pum_postalcodes` (
+								`frompc` varchar(5) NOT NULL,
+								`topc` varchar(5) NOT NULL,
+								`time` varchar(8) NOT NULL,
+								`distance` varchar(8) NOT NULL
+							) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 		
-		$odb = "devcivi_civicrm";
-		$otbl = "pum_postalcodes";
-		$ndb = "devcivi_civicrm";
-		$ntbl = "pum_postalcode_".$dtstamp;
+		$eqRenameTable_pc = CRM_Core_DAO::executeQuery($qRenameTable_pc); 
+		$eqCreateTable_pc = CRM_Core_DAO::executeQuery($qCreateTable_pc);
 		
-		$qsql_0 = CRM_Core_DAO::executeQuery($sql_0);
-		$fsql_0 = $qsql_0->fetchAll();
-		dpm($fsql_0);
-		
-		/*
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {	
-			if ($data[0]== "Frompc4") {
-				continue;		//first line, so skip iteration
+		/**
+		 * @todo 
+		 * Method now used to skip the first line not very beautiful, maybe PHP has a function for it?
+		 * 
+		 */
+		 
+		$i = 0;
+		$rows = 0;
+		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			if ($i == 0) {
+				$i++;
+				continue;	//We have to skip the first line
+			}
+						
+			
+			$num = count($data);
+			
+			echo "<p> $num fields in line $i: <br /></p>\n";
+			
+			for ($k = 0; $k < $num; $k++) {
+				echo $data[$k] . "<br />\n";
 			}
 			
-			$sql_3 = "INSERT INTO devcivi_civicrm.pum_postalcodes (frompc, topc, time, distance) VALUES (%1,%2,%3,%4)";
-			$sql_3_v = array( 	0 => array($data[0]),
-								1 => array($data[1]),
-								2 => array($data[2]),
-								3 => array($data[3]),
+			$qInsert_pc = "INSERT INTO nl_pum_postalcodes (frompc, topc, time, distance) VALUES (%0,%1,%2,%3)";
+			
+			$qInsert_pc_v = array(
+						 array($data[0], 'String'),
+						 array($data[1], 'String'),
+						 array($data[2], 'String'),
+						 array($data[3], 'String'),
 						);
-			$rsql_3 = CRM_Core_DAO::executeQuery($sql_3,$sql_3_v);
-		
-			dpm($data);
-			
-			/*foreach ($data as $distance) {
-				dpm($distance);
-				$rows++;
-			}
-			*
+						
+			$eqInsert_pc = CRM_Core_DAO::executeQuery($qInsert_pc,$qInsert_pc_v);
+			$rows++;
 		}
-		*/
+		drupal_set_message($rows." rows imported succesfully!");
+		
 		fclose($handle);
-		//dpm($postal_codes);
-		//dpm($rows.' rows imported!');
    	}
    	
    	parent::run();
